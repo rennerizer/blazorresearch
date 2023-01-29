65fa828e857b7280aa48831a04b7ac0dca380b7b
rennerizer <rennerizer@gmail.com>
2023-01-28 20:17:16 -0700

    Added PWA project with external service calls

---
+++ Packt-BlazorWebAssemblyByExample/WeatherForecast/WeatherForecast/Models/OpenWeather.cs
@@ -0,0 +1,30 @@
+﻿namespace WeatherForecast.Models
+{
+    public class OpenWeather
+    {
+        public Daily[] Daily { get; set; } = default!;
+    }
+
+    public class Daily
+    {
+        public long Dt { get; set; }
+
+        public Temp Temp { get; set; } = default!;
+
+        public Weather[] Weather { get; set; } = default!;
+    }
+
+    public class Temp
+    {
+        public double Min { get; set; }
+
+        public double Max { get; set; }
+    }
+
+    public class Weather
+    {
+        public string Description { get; set; } = string.Empty;
+
+        public string Icon { get; set; } = string.Empty;
+    }
+}
