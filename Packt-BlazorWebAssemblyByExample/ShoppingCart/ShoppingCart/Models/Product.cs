d66229e79b7e0706f93dcb895414a186a654a156
rennerizer <rennerizer@gmail.com>
2023-02-02 20:38:51 -0700

    Added project showing application state and dependency injection

---
+++ Packt-BlazorWebAssemblyByExample/ShoppingCart/ShoppingCart/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoppingCart.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string? ProductName { get; set; }
+
+        public int Price { get; set; }
+
+        public string? Image { get; set; }
+    }
+}
---
+++ Packt-BlazorWebAssemblyByExample/ShoppingCart/ShoppingCart/Services/CartService.cs
@@ -0,0 +1,35 @@
+﻿using ShoppingCart.Models;
+
+namespace ShoppingCart.Services
+{
+    public class CartService : ICartService
+    {
+        private List<Product> _cart = new();
+        public IList<Product> Cart { get { return _cart; } }
+
+        private int _total;
+        public int Total { get { return _total; } }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AddProduct(Product product)
+        {
+            _cart.Add(product);
+
+            _total += product.Price;
+
+            NotifyStateChanged();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _cart.Remove(product);
+
+            _total -= product.Price;
+
+            NotifyStateChanged();
+        }
+    }
+}
