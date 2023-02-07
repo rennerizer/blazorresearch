99180979bd6ef1a91aa88c875510ba61b038e3a5
rennerizer <rennerizer@gmail.com>
2023-02-07 12:47:00 -0700

    Added a hosted project that works with a back-end web api

---
+++ Packt-BlazorWebAssemblyByExample/TaskManager/TaskManager/Server/Data/TaskManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Shared;
+
+namespace TaskManager.Server.Data
+{
+    public class TaskManagerContext : DbContext
+    {
+        public TaskManagerContext (DbContextOptions<TaskManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskManager.Shared.TaskItem> TaskItem { get; set; } = default!;
+    }
+}
